9f77ef6ad6414f3b5ba38c4671097c622619e0e4
Therese Dachille <tcd12@pitt.edu>
2016-03-22 17:13:57 -0400

    commented on Other.java

--- Other.java
+++ Other.java
@@ -3,30 +3,31 @@ public class Other {
     private int val;
 
     public Other(int val) {
-	this.val = val;
+		this.val = val;
     }
 
+	//returns "MidnightMidnight2Midnight22Midnight222Midnight2222..."
     public String levelifier(int val, String toReturn) {
-	Other o = new Other(val % 13);
-	if (val < 1500) {
-	    return toReturn + o.levelifier(++val, toReturn + "2");
-	} else {
-	    return "3";
-	}
+		Other o = new Other(val % 13);
+		if (val < 1500) {
+			return toReturn + o.levelifier(++val, toReturn + "2");
+		} else {
+			return "3";
+		}
     }
     
     public String threatLevel(int val) {
-	String toReturn = "Midnight";
-	String conv = levelifier(val, toReturn).substring(0, Math.abs(val));
-	return conv;
+		String toReturn = "Midnight";
+		String conv = levelifier(val, toReturn).substring(0, Math.abs(val));	//takes first 'val' characters of whatever levelifier returns
+		return conv;
     }
 
     public long defcon(long val) {
-	val = (val > 2000) ? 2000 : val;
-	if (val > 0) {
-	    return val + defcon(val - 1);
-	} else {
-	    return val;
-	}
+		val = (val > 2000) ? 2000 : val;
+		if (val > 0) {
+			return val + defcon(val - 1);
+		} else {
+			return val;
+		}
     }
 }
