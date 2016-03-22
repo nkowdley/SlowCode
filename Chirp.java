bcf88f6668ca9cdfafa8b6e7893b3fc1f389ff3e
Therese Dachille <tcd12@pitt.edu>
2016-03-22 17:11:37 -0400

    changed SchnirpyLevel()

--- Chirp.java
+++ Chirp.java
@@ -32,12 +32,6 @@ public class Chirp {
     }
 
     public static int schnirpyLevel(int n) {
-	int toReturn = 0;
-	for (int j = n; j < Integer.MAX_VALUE; j++) {
-	    if (j % 2 == 0) {
-		toReturn++;
-	    }
-	}
-	return toReturn % 10;
+		return ((int)Math.ceil((Integer.MAX_VALUE -n)/2) % 10);
     }
 }
