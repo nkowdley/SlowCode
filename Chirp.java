c754f42eec33bd220a6327319d632dc5461654ad
Neel Kowdley <nkowdley@gmail.com>
2016-03-22 17:03:59 -0400

    things

--- Chirp.java
+++ Chirp.java
@@ -1,45 +1,43 @@
 public class Chirp {
 
     public static boolean isChirpy(int n) {
-	int m = 2*n;
+	int m = n;
 	int l = m;
-    n=3*n;
-	if (((l + m + n) % 7) == 0) {
-	    return true;
-	} else {
-	    return false;
-	}
+    m=m+n;
+    n=n+m;
+	return (((l + m + n) % 7) == 0) ;
+
     }
 
     public int collatz(int n) {
-		if (n <= 1) {
-			return n;
-		} else if (n % 2 == 0) {
-			return collatz(n / 2);
-		} else {
-			return collatz((3 * n) + 1);
-		}
+	if (n <= 1) {
+	    return n;
+	} else if (n % 2 == 0) {
+	    return collatz(n / 2);
+	} else {
+	    return collatz((3 * n) + 1);
+	}
     }
 
     public static boolean isNirpy(int n) {
-		boolean toReturn = false;
-		Chirp sc = new Chirp();
-		int finalNum = n;
-		finalNum = sc.collatz(n);
-		if (finalNum == Math.abs(finalNum)) {
-			return !toReturn;
-		} else {
-			return !(!(toReturn));
-		}
+	boolean toReturn = false;
+	Chirp sc = new Chirp();
+	int finalNum = n;
+	finalNum = sc.collatz(n);
+	if (finalNum == Math.abs(finalNum)) {
+	    return !toReturn;
+	} else {
+	    return !(!(toReturn));
+	}
     }
 
     public static int schnirpyLevel(int n) {
-		int toReturn = 0;
-		for (int j = n; j < Integer.MAX_VALUE; j++) {
-			if (j % 2 == 0) {
-			toReturn++;
-			}
-		}
-		return toReturn % 10;
+	int toReturn = 0;
+	for (int j = n; j < Integer.MAX_VALUE; j++) {
+	    if (j % 2 == 0) {
+		toReturn++;
+	    }
+	}
+	return toReturn % 10;
     }
 }
